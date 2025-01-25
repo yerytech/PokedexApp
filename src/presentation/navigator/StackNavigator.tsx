e9075141d4c1eae1a6e38c0907cefec3e072b210
yerytech <yerytecnology@gmail.com>
2025-01-25 14:05:45 -0400

    Implement navigation structure with StackNavigator and update PokedexApp

--- src/presentation/navigator/StackNavigator.tsx
+++ src/presentation/navigator/StackNavigator.tsx
@@ -1,8 +1,25 @@
+import { createStackNavigator } from "@react-navigation/stack";
 import { Text, View } from "react-native";
+import { HomeScreen, PokemonScreen, SearchScreen } from "../screen";
+const Stack = createStackNavigator();
 export const StackNavigator = () => {
   return (
-    <View>
-      <Text>StackNavigator</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen
+        name={"HomeScreen"}
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name={"PokemonScreen"}
+        component={PokemonScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name={"SearchScreen"}
+        component={SearchScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
   );
 };
---
+++ src/presentation/screen/index.ts
@@ -0,0 +1,3 @@
+export * from "../screen/home/HomeScreen";
+export * from "../screen/pokemon/PokemonScreen";
+export * from "../screen/search/SearchScreen";
