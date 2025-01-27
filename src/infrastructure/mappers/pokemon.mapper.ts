440e6ed23daf35a967aaaf4d19a464c77d31f0fb
yerytech <yerytecnology@gmail.com>
2025-01-27 08:59:21 -0400

    Implement PokemonMapper to transform PokeAPI data into domain entities with sprite handling

--- src/infrastructure/mappers/pokemon.mapper.ts
+++ src/infrastructure/mappers/pokemon.mapper.ts
@@ -1,1 +1,42 @@
-export class PokemonMapper {}
+import { Pokemon } from "../../domain/entities/pokemon";
+import { PokeAPIPokemons } from "../interfaces/pokeApi.interfaces";
+
+export class PokemonMapper {
+  static pokeApiPokemonToEntity(data: PokeAPIPokemons): Pokemon {
+    const sprites = PokemonMapper.getSprites(data);
+    const avatar = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
+    return {
+      id: data.id,
+      name: data.name,
+      avatar: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
+      sprites: [
+        data.sprites.back_default,
+        data.sprites.front_default,
+        data.sprites.front_shiny,
+        data.sprites.back_shiny,
+      ],
+      types: data.types.map((type) => type.type.name),
+    };
+  }
+  static getSprites(data: PokeAPIPokemons): string[] {
+    const sprites: string[] = [
+      data.sprites.front_default,
+      data.sprites.back_default,
+      data.sprites.front_shiny,
+      data.sprites.back_shiny,
+    ];
+
+    if (data.sprites.other?.home.front_default)
+      sprites.push(data.sprites.other?.home.front_default);
+    if (data.sprites.other?.["official-artwork"].front_default)
+      sprites.push(data.sprites.other?.["official-artwork"].front_default);
+    if (data.sprites.other?.["official-artwork"].front_shiny)
+      sprites.push(data.sprites.other?.["official-artwork"].front_shiny);
+    if (data.sprites.other?.showdown.front_default)
+      sprites.push(data.sprites.other?.showdown.front_default);
+    if (data.sprites.other?.showdown.back_default)
+      sprites.push(data.sprites.other?.showdown.back_default);
+
+    return sprites;
+  }
+}
