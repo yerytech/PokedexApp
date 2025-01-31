996ce2132908370405f217dd4b44cf20c62fe38b
yerytech <yerytecnology@gmail.com>
2025-01-31 09:38:40 -0400

    Implement search functionality in SearchScreen with TextInput, ActivityIndicator, and FlatList for Pokémon display

--- src/presentation/screen/search/SearchScreen.tsx
+++ src/presentation/screen/search/SearchScreen.tsx
@@ -1,8 +1,30 @@
-import { Text, View } from "react-native";
+import { ActivityIndicator, FlatList, View } from "react-native";
+import { globalTheme } from "../../../config/theme/global-theme";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { TextInput } from "react-native-paper";
+import { PokemonCard } from "../../components/pokemons/PokemonCard";
+import { Pokemon } from "../../../domain/entities/pokemon";
 export const SearchScreen = () => {
+  const { top } = useSafeAreaInsets();
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={[globalTheme.globalMargin, { marginTop: top }]}>
+      <TextInput
+        placeholder="Buscar Pokemon"
+        mode="flat"
+        autoFocus
+        autoCorrect={false}
+        onChangeText={(value) => console.log(value)}
+        value="hola"
+      />
+      <ActivityIndicator style={{ paddingTop: 20 }} />
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        style={{ paddingTop: top + 20 }}
+        data={[] as Pokemon[]}
+        keyExtractor={(pokemon, i) => `${pokemon.id}-${i}`}
+        numColumns={2}
+        renderItem={({ item }) => <PokemonCard pokemon={item} />}
+      />
     </View>
   );
 };
