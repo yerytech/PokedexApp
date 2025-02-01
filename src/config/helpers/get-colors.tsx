c13c4385c18919075480fbad1099bca121db0da4
yerytech <yerytecnology@gmail.com>
2025-02-01 13:01:40 -0400

    update get-colors.tsx

--- src/config/helpers/get-colors.tsx
+++ src/config/helpers/get-colors.tsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+import { getColors, ImageColorsResult } from "react-native-image-colors";
+
 export const getColorsFromImage = async (image: string) => {
-  const fallbackColors = "grey";
+  // const [colors, setColors] = useState<ImageColorsResult | null>(null);
+  const fallbackColors = "#228B22";
+
+  // useEffect(() => {
+  //   getColors(image, {
+  //     fallback: fallbackColors,
+  //     cache: true,
+  //     key: image,
+  //   }).then((colors) => setColors(colors));
+  // }, []);
+
   return fallbackColors;
 };
