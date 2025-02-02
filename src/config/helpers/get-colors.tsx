54ce78abd6d1ccff2d9ec5e7fff40fae5f0aaa9d
yerytech <yerytecnology@gmail.com>
2025-02-01 20:41:01 -0400

    Update lastUsed timestamp in devices.json and refactor getColorsFromImage function

--- src/config/helpers/get-colors.tsx
+++ src/config/helpers/get-colors.tsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from "react";
-import { getColors, ImageColorsResult } from "react-native-image-colors";
+import { getColors } from "react-native-image-colors";
 
 export const getColorsFromImage = async (image: string) => {
-  // const [colors, setColors] = useState<ImageColorsResult | null>(null);
   const fallbackColors = "#228B22";
 
-  // useEffect(() => {
-  //   getColors(image, {
-  //     fallback: fallbackColors,
-  //     cache: true,
-  //     key: image,
-  //   }).then((colors) => setColors(colors));
-  // }, []);
+  const colors = await getColors(image, {
+    fallback: fallbackColors,
+    // cache: true,
+    key: image,
+  });
 
-  return fallbackColors;
+  switch (colors.platform) {
+    case "android":
+      return colors.dominant || fallbackColors;
+
+    case "ios":
+      return colors.background || fallbackColors;
+
+    default:
+      return fallbackColors;
+  }
 };
