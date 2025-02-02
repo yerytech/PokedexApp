77c5aa955251553a080554a9ae557a724d5d4e0c
yerytech <yerytecnology@gmail.com>
2025-02-01 20:24:14 -0400

    Update device configuration and downgrade NDK version in build.gradle

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StackNavigator } from "./presentation/navigator/StackNavigator";
-import { PaperProvider } from "react-native-paper";
 import { ThemeContextProvider } from "./presentation/context/ThemeContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
